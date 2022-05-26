c07385bd5ff9af5f72ada155ab59adf4fed5c334
ying_123 <zhangying@aten-optics.com>
2022-05-26 12:39:56 +0800

    update downloader

--- util/m3u8.ts
+++ util/m3u8.ts
@@ -1,7 +1,7 @@
 import { saveAs } from './FileSaver'
 
 export function createDownloadBat(url: string, title: string): void {
-    const cmd = `@echo off\nN_m3u8DL-CLI_v2.9.9.exe "${url}" --workDir "./Downloads" --saveName "${encodeURIComponent(title)}" --enableDelAfterDone`
+    const cmd = `@echo off\nN_m3u8DL-CLI.exe "${url}" --workDir "./Downloads" --saveName "${encodeURIComponent(title)}" --enableDelAfterDone`
     const blob = new Blob([cmd], {
         type: 'text/plain;charset=ANSI',
         endings: 'native'
