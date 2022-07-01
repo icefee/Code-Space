3c859b1cbf65221d86cb78244c888feca54af84e
ying_123 <zhangying@aten-optics.com>
2022-07-01 11:00:45 +0800

    fix bat permission

--- util/tile.ts
+++ util/tile.ts
@@ -1,7 +1,13 @@
 import { blobToFile } from './blobToFile'
 
+/*
+@echo off&color 2
+%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
+cd /d "%~dp0"
+*/
+
 export function createTileBat(args: string, title: string): void {
-    const cmd = `@echo off\ntile.exe ${args}\npause`
+    const cmd = `@echo off&color 2\n%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit\ncd /d "%~dp0"\ntile.exe ${args}\npause`
     const blob = new Blob([cmd], {
         type: 'text/plain;charset=ANSI',
         endings: 'native'
