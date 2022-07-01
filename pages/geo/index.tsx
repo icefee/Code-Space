bfff5089ebd8d71020fd30b810db26eb802b78ef
icefee <icefee@live.cn>
2022-07-02 00:42:46 +0800

    提高了兼容性。

--- pages/geo/index.tsx
+++ pages/geo/index.tsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { TextField, Button } from '@mui/material'
-import css from './style.module.css'
+import { useCopyToClipboard } from "react-use";
+import css from './style.module.css';
+import { SnackbarProvider, useSnackbar } from "util/useSnackbar";
+
+export function CopyListener({ text }) {
+    const [, setClipboard] = useCopyToClipboard();
+    const showSnackbar = useSnackbar();
+    useEffect(() => {
+        if (text !== '') {
+            setClipboard(text);
+            showSnackbar({
+                message: '转换结果已经复制到剪切板, 可直接粘贴',
+                autoHideDuration: 3000
+            })
+        }
+    }, [text])
+    return null
+}
 
 class Geo extends React.Component {
 
@@ -10,20 +27,21 @@ class Geo extends React.Component {
     }
 
     public transformGeo(): void {
-        const {  origin } = this.state
+        const { origin } = this.state
         const dataList = origin.split(
             /\n/g
         )
         if (dataList.length > 0) {
             const result = dataList.map(
                 line => line.replace(
-                    /\d{0,3}°\d{0,2}′\d{0,2}(\.\d+)?″/g,
+                    /(\d{1,3}°)?(\d{1,2}′)?(\d{1,2}(\.\d+)?″)?/g,
                     v => {
-                        const h = v.match(/^\d{0,3}/)
-                        const m = v.match(/(?<=°)\d{0,3}(?=′)/)
-                        const s = v.match(/(?<=′)\d{0,2}(\.\d+)?(?=″)/)
+                        const h = v.match(/^\d{1,3}(?=°)/)
+                        const m = v.match(/((?<=°)|^)\d{1,3}(?=′)/)
+                        const s = v.match(/((?<=(°|′))|^)\d{1,2}(\.\d+)?(?=″)/)
                         const valid: (arg: string[] | null) => number = v => v ? Number(v[0]) : 0;
-                        return String(valid(h) + valid(m) / 60 + valid(s) / 3600)
+                        const r = valid(h) + valid(m) / 60 + valid(s) / 3600;
+                        return r > 0 ? String(r) : ''
                     }
                 )
             ).join('\n')
@@ -42,19 +60,34 @@ class Geo extends React.Component {
 
     render() {
         return (
-            <div className={css.container}>
-                <div className={css.header}>
-                    <h3>经纬度转换(时分秒转小数)</h3>
+            <SnackbarProvider>
+                <div className={css.container}>
+                    <div className={css.header}>
+                        <h3>经纬度转换(时分秒转小数)</h3>
+                    </div>
+                    <div className={css.fields}>
+                        <TextField
+                            multiline
+                            label="经纬度"
+                            placeholder="支持多行"
+                            value={this.state.origin}
+                            onChange={ev => this.setState({ origin: ev.target.value })}
+                            rows={20}
+                        />
+                        <TextField
+                            multiline
+                            label="转换结果"
+                            value={this.state.result}
+                            rows={20}
+                        />
+                        <CopyListener text={this.state.result} />
+                    </div>
+                    <div className={css.actions}>
+                        <Button variant="contained" onClick={this.transformGeo.bind(this)}>转换</Button>
+                        <Button onClick={this.clearFields.bind(this)}>清空</Button>
+                    </div>
                 </div>
-                <div className={css.fields}>
-                    <TextField multiline label="经纬度" value={this.state.origin} onChange={ev => this.setState({ origin: ev.target.value})} rows={20} />
-                    <TextField multiline label="转换结果" value={this.state.result} rows={20} />
-                </div>
-                <div className={css.actions}>
-                    <Button variant="contained" onClick={this.transformGeo.bind(this)}>转换</Button>
-                    <Button onClick={this.clearFields.bind(this)}>清空</Button>
-                </div>
-            </div>
+            </SnackbarProvider>
         )
     }
 }
